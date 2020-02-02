b9d48748fd823702739bac693ee6b4df71183148
abrie <abrie@users.noreply.github.com>
2020-02-02 13:57:06 -0500

    Remove deprecated utility scripts.

--- migration-utils/flatten-data.js
+++
@@ -1,7 +0,0 @@
-const input = require('./migrated/data.json')
-
-const flattened = Object.entries(input)
-  .reduce( (acc, [key,  obj]) =>
-    Object.assign(acc, {[key]:[... Object.values(obj)]}), {});
-
-console.log(JSON.stringify(flattened));
